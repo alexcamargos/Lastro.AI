91e0c3939ad0585f32e3a45fce7e18a85c600b8c
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2025-12-24 15:43:20 -0300

    feat(web): cria estrutura inicial do módulo web

---
+++ lastro_ai/web/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+#  Name: __init__.py
+#  Version: 0.0.1
+#
+#  Summary: Lastro.AI: Seu Agente de Política Monetária.
+#           Transforma relatórios estáticos do BACEN em conhecimento
+#           dinâmico e preciso.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alexcamargos@gmail.com
+#
+#  License: MIT
+# ------------------------------------------------------------------------------
